a395646f66742ab31b169bc381d3196e965261a6
Clelo-567 <18318912888@163.com>
2021-01-14 15:16:36 +0800

    lst-新增换料QC模块

--- js/api.js
+++ js/api.js
@@ -1,10 +1,10 @@
 var API = window.API = {
-	webPath			: "http://192.168.1.246:8080/jyapp/",
-	//webPath			: "http://10.1.12.76:9999/",
+	//webPath			: "http://192.168.1.160:8989/zrt/", //160-test
+	//webPath			: "http://10.168.0.69:8080/zrt/",//zrt-official
 	//webPath			: "http://10.1.1.102:8080/jyapp/",
 	//webPath			: "http://192.168.1.160:9998/jyapp/",
 	//webPath			: "http://192.168.1.26:9998/jyapp/",
-	//webPath			: "http://192.168.0.102:9999/",
+	webPath			: "http://192.168.0.106:8080/zrt",//localhost
 	//webPath			: "http://192.168.2.193:9999/jyapp/",
 	version         :"test",//demo:静态，prod:正式，test:测试
 	env:'soap',//只支持web-service(soap)或者json
